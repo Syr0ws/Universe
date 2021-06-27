bf117ad5b6d4133b3e92e2b273911b9903117537
cleme <clement.gho@gmail.com>
2021-06-27 19:23:56 +0200

    Adding an AttributeObservable implementation to avoid code duplication when using it.

--- src/main/java/com/github/syr0ws/universe/attributes/AbstractAttributeObservable.java
+++ src/main/java/com/github/syr0ws/universe/attributes/AbstractAttributeObservable.java
@@ -5,16 +5,18 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
-public class AttributeObservable {
+public abstract class AbstractAttributeObservable implements AttributeObservable {
 
     private final List<AttributeObserver> observers = new ArrayList<>();
 
+    @Override
     public void notifyChange(Attribute attribute) {
         new ArrayList<>(this.observers).stream()
                 .filter(observer -> observer.observed().contains(attribute))
                 .forEach(observer -> observer.onUpdate(attribute));
     }
 
+    @Override
     public void addObserver(AttributeObserver observer) {
 
         if(this.observers.contains(observer))
@@ -23,14 +25,17 @@ public class AttributeObservable {
         this.observers.add(observer);
     }
 
+    @Override
     public void removeObserver(AttributeObserver observer) {
         this.observers.remove(observer);
     }
 
+    @Override
     public boolean hasObserver(AttributeObserver observer) {
         return this.observers.contains(observer);
     }
 
+    @Override
     public Collection<AttributeObserver> getObservers() {
         return Collections.unmodifiableList(this.observers);
     }
