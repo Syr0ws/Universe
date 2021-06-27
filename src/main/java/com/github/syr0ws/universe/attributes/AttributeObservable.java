8f8afbf7265e7a6ddf821ada5115c3377d9eeefa
cleme <clement.gho@gmail.com>
2021-06-27 19:24:21 +0200

    Adding interface.

---
+++ src/main/java/com/github/syr0ws/universe/attributes/AttributeObservable.java
@@ -0,0 +1,16 @@
+package com.github.syr0ws.universe.attributes;
+
+import java.util.Collection;
+
+public interface AttributeObservable {
+
+    void notifyChange(Attribute attribute);
+
+    void addObserver(AttributeObserver observer);
+
+    void removeObserver(AttributeObserver observer);
+
+    boolean hasObserver(AttributeObserver observer);
+
+    Collection<AttributeObserver> getObservers();
+}
