82bc0ae5f18fcc812bd8ecd8c751c0e9f5592dbf
cleme <clement.gho@gmail.com>
2021-06-28 22:26:38 +0200

    Adding setMode() method.

--- src/main/java/com/github/syr0ws/universe/game/controller/GameController.java
+++ src/main/java/com/github/syr0ws/universe/game/controller/GameController.java
@@ -1,8 +1,12 @@
 package com.github.syr0ws.universe.game.controller;
 
 import com.github.syr0ws.universe.game.model.GameException;
+import com.github.syr0ws.universe.game.model.GamePlayer;
+import com.github.syr0ws.universe.game.model.Mode;
 
 public interface GameController {
+
+    void setMode(GamePlayer player, Mode mode);
 
     void startGame() throws GameException;
 
