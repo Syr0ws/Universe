e02931712ed4096b173b75514cd4eb9445bb8cc3
cleme <clement.gho@gmail.com>
2021-06-28 18:06:40 +0200

    Adding methods.

--- src/main/java/com/github/syr0ws/universe/game/model/GameModel.java
+++ src/main/java/com/github/syr0ws/universe/game/model/GameModel.java
@@ -3,6 +3,10 @@ package com.github.syr0ws.universe.game.model;
 import com.github.syr0ws.universe.attributes.AttributeObservable;
 import com.github.syr0ws.universe.game.model.cycle.GameCycle;
 
+import java.util.Collection;
+import java.util.Optional;
+import java.util.UUID;
+
 public interface GameModel extends AttributeObservable {
 
     GameCycle getCycle();
@@ -12,4 +16,12 @@ public interface GameModel extends AttributeObservable {
     boolean isStarted();
 
     boolean isFinished();
+
+    GamePlayer getPlayer(UUID uuid);
+
+    Optional<? extends GamePlayer> getPlayer(String name);
+
+    Collection<? extends GamePlayer> getPlayers();
+
+    Collection<? extends GamePlayer> getOnlinePlayers();
 }
