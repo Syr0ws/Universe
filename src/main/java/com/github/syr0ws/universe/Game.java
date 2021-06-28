82a99ebbd0a69388af849ff72a367db2696efd91
cleme <clement.gho@gmail.com>
2021-06-28 17:58:09 +0200

    Adding model and controller.

--- src/main/java/com/github/syr0ws/universe/Game.java
+++ src/main/java/com/github/syr0ws/universe/Game.java
@@ -1,11 +1,13 @@
 package com.github.syr0ws.universe;
 
+import com.github.syr0ws.universe.game.controller.GameController;
+import com.github.syr0ws.universe.game.model.GameModel;
 import com.github.syr0ws.universe.listeners.ListenerManager;
 import com.github.syr0ws.universe.modules.GameModuleService;
 import com.github.syr0ws.universe.modules.ModuleService;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Game extends JavaPlugin {
+public abstract class Game extends JavaPlugin {
 
     private final ModuleService service;
     private final ListenerManager listenerManager;
@@ -14,6 +16,10 @@ public class Game extends JavaPlugin {
         this.service = new GameModuleService();
         this.listenerManager = new ListenerManager(this);
     }
+
+    public abstract GameModel getGameModel();
+
+    public abstract GameController getGameController();
 
     public ListenerManager getListenerManager() {
         return this.listenerManager;
