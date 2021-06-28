55bfd4169d1527f93b26533039c0084cfd47640b
cleme <clement.gho@gmail.com>
2021-06-28 17:58:20 +0200

    Removing model.

--- src/main/java/com/github/syr0ws/universe/game/controller/GameController.java
+++ src/main/java/com/github/syr0ws/universe/game/controller/GameController.java
@@ -1,13 +1,10 @@
 package com.github.syr0ws.universe.game.controller;
 
 import com.github.syr0ws.universe.game.model.GameException;
-import com.github.syr0ws.universe.game.model.GameModel;
 
 public interface GameController {
 
     void startGame() throws GameException;
 
     void stopGame() throws GameException;
-
-    GameModel getGameModel();
 }
