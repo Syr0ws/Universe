cf8d68a876155d7a67388fefb0b8f003fcfe1bf0
cleme <clement.gho@gmail.com>
2021-07-01 15:49:13 +0200

    Modifying setMode() method.

--- src/main/java/com/github/syr0ws/universe/game/controller/GameController.java
+++ src/main/java/com/github/syr0ws/universe/game/controller/GameController.java
@@ -1,12 +1,12 @@
 package com.github.syr0ws.universe.game.controller;
 
 import com.github.syr0ws.universe.game.model.GameException;
-import com.github.syr0ws.universe.game.model.mode.Mode;
-import org.bukkit.entity.Player;
+import com.github.syr0ws.universe.game.model.GamePlayer;
+import com.github.syr0ws.universe.game.model.mode.ModeType;
 
 public interface GameController {
 
-    void setMode(Player player, Mode mode);
+    void setMode(GamePlayer player, ModeType type);
 
     void startGame() throws GameException;
 
