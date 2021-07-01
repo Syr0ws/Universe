4a7ab5d2b13a5d4cbbc10b61855dcd2c574e411a
cleme <clement.gho@gmail.com>
2021-07-01 12:21:01 +0200

    Adding a reusable placeholder enum.

---
+++ src/main/java/com/github/syr0ws/universe/displays/placeholders/PlaceholderEnum.java
@@ -0,0 +1,17 @@
+package com.github.syr0ws.universe.displays.placeholders;
+
+public enum PlaceholderEnum implements Placeholder {
+
+    PLAYER_NAME("player"), DISPLAY_NAME("display_name"), MESSAGE("message");
+
+    private final String placeholder;
+
+    PlaceholderEnum(String placeholder) {
+        this.placeholder = placeholder;
+    }
+
+    @Override
+    public String get() {
+        return "%" + this.placeholder + "%";
+    }
+}
