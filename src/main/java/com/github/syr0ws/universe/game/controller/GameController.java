f73dbe7ea1250e26e5db98dd5bbf1775fccc9cc4
cleme <clement.gho@gmail.com>
2021-07-01 11:00:47 +0200

    Changing GamePlayer to Player in setMode() method.

--- src/main/java/com/github/syr0ws/universe/game/controller/GameController.java
+++ src/main/java/com/github/syr0ws/universe/game/controller/GameController.java
@@ -1,12 +1,12 @@
 package com.github.syr0ws.universe.game.controller;
 
 import com.github.syr0ws.universe.game.model.GameException;
-import com.github.syr0ws.universe.game.model.GamePlayer;
 import com.github.syr0ws.universe.game.model.Mode;
+import org.bukkit.entity.Player;
 
 public interface GameController {
 
-    void setMode(GamePlayer player, Mode mode);
+    void setMode(Player player, Mode mode);
 
     void startGame() throws GameException;
 
