beeadf530d2fa8899004f3edcf0ae866e49ef6b9
cleme <clement.gho@gmail.com>
2021-07-06 17:57:18 +0200

    Now extending the Module interface.

--- src/main/java/com/github/syr0ws/universe/modules/combat/CombatModule.java
+++ src/main/java/com/github/syr0ws/universe/modules/combat/CombatModule.java
@@ -1,8 +1,9 @@
 package com.github.syr0ws.universe.modules.combat;
 
+import com.github.syr0ws.universe.modules.Module;
 import com.github.syr0ws.universe.modules.combat.settings.CombatSettings;
 
-public interface CombatModule {
+public interface CombatModule extends Module {
 
     CombatService getService();
 
