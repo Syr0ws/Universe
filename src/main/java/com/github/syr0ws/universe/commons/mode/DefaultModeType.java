e7f9594146065959daac3d037e31e3db1fe061ec
cleme <clement.gho@gmail.com>
2021-07-20 21:00:51 +0200

    Moving to another package. Removing "ADMIN" value.

---
+++ src/main/java/com/github/syr0ws/universe/commons/mode/DefaultModeType.java
@@ -0,0 +1,8 @@
+package com.github.syr0ws.universe.commons.mode;
+
+import com.github.syr0ws.universe.sdk.game.model.mode.ModeType;
+
+public enum DefaultModeType implements ModeType {
+
+    WAITING, PLAYING, SPECTATOR;
+}
