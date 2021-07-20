b89ba39584e976e21e8641a0da356758eff8dbb9
cleme <clement.gho@gmail.com>
2021-07-20 21:37:07 +0200

    Now an interface.

--- src/main/java/com/github/syr0ws/universe/sdk/game/model/cycle/GameCycle.java
+++ src/main/java/com/github/syr0ws/universe/sdk/game/model/cycle/GameCycle.java
@@ -1,70 +1,22 @@
 package com.github.syr0ws.universe.sdk.game.model.cycle;
 
+import com.github.syr0ws.universe.sdk.game.model.GameState;
 import com.github.syr0ws.universe.sdk.Game;
 import com.github.syr0ws.universe.sdk.attributes.AbstractAttributeObservable;
 import com.github.syr0ws.universe.sdk.attributes.Attribute;
 import com.github.syr0ws.universe.sdk.listeners.ListenerManager;
 
-public abstract class GameCycle extends AbstractAttributeObservable {
+public interface GameCycle {
 
-    private final Game game;
-    private final ListenerManager listenerManager;
+    GameState getGameState();
 
-    private GameCycleState state;
+    GameCycleState getCycleState();
 
-    public GameCycle(Game game) {
+    void load();
 
-        if(game == null)
-            throw new IllegalArgumentException("Game cannot be null.");
+    void unload();
 
-        this.game = game;
-        this.listenerManager = new ListenerManager(game);
-        this.state = GameCycleState.WAITING;
-    }
+    void start();
 
-    private void setState(GameCycleState state) {
-
-        if(!this.state.isNext(state))
-            throw new GameCycleException(String.format("State '%s' is not the next state of '%s'.", state.name(), this.state.name()));
-
-        this.state = state;
-        this.notifyChange(GameCycleAttribute.CYCLE_STATE_CHANGE);
-    }
-
-    public void load() {
-        this.setState(GameCycleState.LOADED);
-    }
-
-    public void unload() {
-        this.setState(GameCycleState.UNLOADED);
-    }
-
-    public void start() {
-        this.setState(GameCycleState.STARTED);
-    }
-
-    public void stop() {
-        this.setState(GameCycleState.STOPPED);
-    }
-
-    protected void done() {
-        this.notifyChange(GameCycleAttribute.DONE);
-    }
-
-    public Game getGame() {
-        return this.game;
-    }
-
-    public ListenerManager getListenerManager() {
-        return this.listenerManager;
-    }
-
-    public GameCycleState getState() {
-        return this.state;
-    }
-
-    public enum GameCycleAttribute implements Attribute {
-
-        CYCLE_STATE_CHANGE, DONE;
-    }
+    void stop();
 }
