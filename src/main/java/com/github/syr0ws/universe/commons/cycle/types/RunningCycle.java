6d6efa89b25318097faa82170426109841b2a4d6
cleme <clement.gho@gmail.com>
2021-07-22 17:21:26 +0200

    Supporting RunningListener.

--- src/main/java/com/github/syr0ws/universe/commons/cycle/types/RunningCycle.java
+++ src/main/java/com/github/syr0ws/universe/commons/cycle/types/RunningCycle.java
@@ -1,10 +1,12 @@
 package com.github.syr0ws.universe.commons.cycle.types;
 
 import com.github.syr0ws.universe.commons.cycle.DefaultGameCycle;
+import com.github.syr0ws.universe.commons.listeners.RunningListener;
 import com.github.syr0ws.universe.sdk.Game;
 import com.github.syr0ws.universe.sdk.game.controller.GameController;
 import com.github.syr0ws.universe.sdk.game.model.GameModel;
 import com.github.syr0ws.universe.sdk.game.model.GameState;
+import com.github.syr0ws.universe.sdk.listeners.ListenerManager;
 
 public class RunningCycle extends DefaultGameCycle {
 
@@ -13,7 +15,19 @@ public class RunningCycle extends DefaultGameCycle {
     }
 
     @Override
+    public void load() {
+        super.load();
+        this.registerListeners();
+    }
+
+    @Override
     public GameState getGameState() {
         return GameState.RUNNING;
+    }
+
+    private void registerListeners() {
+
+        ListenerManager manager = super.getListenerManager();
+        manager.addListener(new RunningListener(super.getModel()));
     }
 }
