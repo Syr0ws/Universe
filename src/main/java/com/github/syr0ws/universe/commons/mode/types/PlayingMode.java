576aaadf3f1735a75aee6b83543339bf88f9b1d4
cleme <clement.gho@gmail.com>
2021-07-23 15:56:14 +0200

    Actions are now done only if a player is not playing.

--- src/main/java/com/github/syr0ws/universe/commons/mode/types/PlayingMode.java
+++ src/main/java/com/github/syr0ws/universe/commons/mode/types/PlayingMode.java
@@ -3,6 +3,7 @@ package com.github.syr0ws.universe.commons.mode.types;
 import com.github.syr0ws.universe.commons.mode.DefaultModeType;
 import com.github.syr0ws.universe.sdk.game.mode.ModeType;
 import com.github.syr0ws.universe.sdk.game.model.GameModel;
+import com.github.syr0ws.universe.sdk.game.model.GamePlayer;
 import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 
@@ -15,13 +16,18 @@ public class PlayingMode extends DefaultGameMode {
     @Override
     public void enable(Player player) {
 
-        player.setHealth(20);
-        player.setFoodLevel(20);
-        player.setExp(0);
-        player.setLevel(0);
-        player.getInventory().clear();
-        player.setGameMode(GameMode.SURVIVAL);
-        player.setBedSpawnLocation(null);
+        GamePlayer gamePlayer = this.getModel().getPlayer(player.getUniqueId());
+
+        if(!gamePlayer.isPlaying()) {
+
+            player.setHealth(20);
+            player.setFoodLevel(20);
+            player.setExp(0);
+            player.setLevel(0);
+            player.getInventory().clear();
+            player.setGameMode(GameMode.SURVIVAL);
+            player.setBedSpawnLocation(null);
+        }
     }
 
     @Override
