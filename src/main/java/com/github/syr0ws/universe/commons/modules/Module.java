228e188a5cb68f49b0cff5b231efdc2bb7053b4d
Syrows <clement.gho@gmail.com>
2021-08-22 19:09:26 +0200

    Adding load() method.

--- src/main/java/com/github/syr0ws/universe/commons/modules/Module.java
+++ src/main/java/com/github/syr0ws/universe/commons/modules/Module.java
@@ -2,6 +2,8 @@ package com.github.syr0ws.universe.commons.modules;
 
 public interface Module {
 
+    void load();
+
     void enable() throws ModuleException;
 
     void disable() throws ModuleException;
