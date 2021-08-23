51eb1d2df7c84a4987107700d73f03304503cea5
Syrows <clement.gho@gmail.com>
2021-08-23 13:37:19 +0200

    API and SDK are now in two different packages.

---
+++ src/main/java/com/github/syr0ws/universe/api/GamePlugin.java
@@ -0,0 +1,14 @@
+package com.github.syr0ws.universe.api;
+
+import com.github.syr0ws.universe.api.game.controller.GameController;
+import com.github.syr0ws.universe.api.game.model.GameModel;
+import com.github.syr0ws.universe.api.modules.ModuleService;
+
+public interface GamePlugin {
+
+    ModuleService getModuleService();
+
+    GameModel getGameModel();
+
+    GameController getGameController();
+}
