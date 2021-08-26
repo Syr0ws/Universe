55e9763868402e4909eea60ccf3bd3cf05d13e82
Syrows <clement.gho@gmail.com>
2021-08-26 18:42:35 +0200

    Adding cycle.

---
+++ src/main/java/com/github/syr0ws/universe/sdk/game/cycle/types/LoadingCycle.java
@@ -0,0 +1,19 @@
+package com.github.syr0ws.universe.sdk.game.cycle.types;
+
+import com.github.syr0ws.universe.api.game.controller.GameController;
+import com.github.syr0ws.universe.api.game.model.GameModel;
+import com.github.syr0ws.universe.api.game.model.GameState;
+import com.github.syr0ws.universe.sdk.Game;
+import com.github.syr0ws.universe.sdk.game.cycle.DefaultGameCycle;
+
+public abstract class LoadingCycle extends DefaultGameCycle {
+
+    public LoadingCycle(Game game, GameModel model, GameController controller) {
+        super(game, model, controller);
+    }
+
+    @Override
+    public GameState getGameState() {
+        return GameState.LOADING;
+    }
+}
