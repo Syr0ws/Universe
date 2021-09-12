805e9d956c09d8d9dc96b326089ae016ee6bf05d
Syrows <clement.gho@gmail.com>
2021-09-12 15:01:06 +0200

    Adding a Builder class.

--- src/main/java/com/github/syr0ws/universe/sdk/modules/interaction/settings/InteractionDataSetting.java
+++ src/main/java/com/github/syr0ws/universe/sdk/modules/interaction/settings/InteractionDataSetting.java
@@ -2,6 +2,7 @@ package com.github.syr0ws.universe.sdk.modules.interaction.settings;
 
 import com.github.syr0ws.universe.api.settings.SettingFilter;
 import com.github.syr0ws.universe.sdk.game.settings.types.ConfigSetting;
+import com.github.syr0ws.universe.sdk.game.settings.types.builder.ConfigSettingBuilder;
 import com.github.syr0ws.universe.sdk.modules.interaction.InteractiveData;
 import com.github.syr0ws.universe.sdk.modules.interaction.config.ConfigInteractiveDataDAO;
 import org.bukkit.configuration.ConfigurationSection;
@@ -19,5 +20,22 @@ public class InteractionDataSetting extends ConfigSetting<InteractiveData> {
         InteractiveData data = dao.load(super.getName());
 
         super.setValue(data);
+    }
+
+    public static class Builder extends ConfigSettingBuilder<InteractiveData, InteractionDataSetting, Builder> {
+
+        public Builder(String name, InteractiveData defaultValue, String path) {
+            super(name, defaultValue, path);
+        }
+
+        @Override
+        public Builder self() {
+            return this;
+        }
+
+        @Override
+        public InteractionDataSetting build() {
+            return new InteractionDataSetting(super.getName(), super.getDefaultValue(), super.getValue(), super.getFilter(), super.getPath());
+        }
     }
 }
